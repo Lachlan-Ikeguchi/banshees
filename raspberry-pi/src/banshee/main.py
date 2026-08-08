7b0b12aeceacf3aee26fed94436f23b4508d7c60
Liam <liamschaaky@gmail.com>
2026-08-08 17:22:09 +1000

    read_adc exists

--- raspberry-pi/src/banshee/main.py
+++ raspberry-pi/src/banshee/main.py
@@ -1,5 +1,17 @@
 import spidev
 
+def read_adc(channel):
+    if channel < 0 or channel > 7: 
+        raise ValueError("Channel must be between 0 and 8")
+
+    command = [1, (8 + channel) << 4, 0]
+
+    response = spi.xfer2(command)
+
+    # Convert the two returned bytes into a 10-bit ADC value
+    value = ((response[1] & 0x03) << 8) | response[2]
+
+    return value
 
 def main() -> None:
     # Open SPI bus 0, device 0 (CE0)
@@ -26,24 +38,8 @@ def main() -> None:
     finally:
         spi.close()
 
+        
 
-import spidev
-
-# open SPI bus 0
-spi = spidev.SpiDev() 
-spi.open(0, 0)
-
-# max speed 200kHz
-spi.max_speed_hz = 200000
-
-def read_adc(channel):
-    if channel < 0 or channel > 3: 
-        raise ValueError("Channel must be between 0 and 3")
-
-    command = [1, (8 + channel) << 4, 0]
-
-def main():
-    print("Hello from pi!")
 
 
 if __name__ == "__main__":
